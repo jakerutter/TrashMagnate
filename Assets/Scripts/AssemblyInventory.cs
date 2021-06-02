cc1819c9ad478a0bff708c0d848735089506ed97
Jake Rutter <rutter.jacob@gmail.com>
2021-06-01 22:46:41 -0700

    pivoting away from rec rockets to rec management

--- Assets/Scripts/AssemblyInventory.cs
+++ Assets/Scripts/AssemblyInventory.cs
@@ -1,85 +1,85 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public static class AssemblyInventory
-{
+// public static class AssemblyInventory
+// {
 
-    private static int PartLimit;
-    private static float MassLimit;
-    private static int BuildSkill;
-    private static Engine[] AllEngines;
-    private static Engine[] UnlockedEngines;
-    private static FuelTank[] AllFuelTanks;
-    private static FuelTank[] UnlockedFuelTanks;
+//     private static int PartLimit;
+//     private static float MassLimit;
+//     private static int BuildSkill;
+//     private static Engine[] AllEngines;
+//     private static Engine[] UnlockedEngines;
+//     private static FuelTank[] AllFuelTanks;
+//     private static FuelTank[] UnlockedFuelTanks;
 
-    public static int GetPartLimit()
-    {
-        return PartLimit;
-    }
+//     public static int GetPartLimit()
+//     {
+//         return PartLimit;
+//     }
 
-    public static void SetPartLimit(int limit)
-    {
-        PartLimit = limit;
-    }
+//     public static void SetPartLimit(int limit)
+//     {
+//         PartLimit = limit;
+//     }
 
-    public static float GetMassLimit()
-    {
-        return MassLimit;
-    }
+//     public static float GetMassLimit()
+//     {
+//         return MassLimit;
+//     }
 
-    public static void SetPartLimit(float limit)
-    {
-        MassLimit = limit;
-    }
+//     public static void SetPartLimit(float limit)
+//     {
+//         MassLimit = limit;
+//     }
 
-    public static int GetBuildSkill()
-    {
-        return BuildSkill;
-    }
+//     public static int GetBuildSkill()
+//     {
+//         return BuildSkill;
+//     }
 
-    public static void AddToBuilSkill(int skill)
-    {
-        BuildSkill += skill;
-    }
+//     public static void AddToBuilSkill(int skill)
+//     {
+//         BuildSkill += skill;
+//     }
 
-    public static Engine[] GetAllEngines()
-    {
-        return AllEngines;
-    }
+//     public static Engine[] GetAllEngines()
+//     {
+//         return AllEngines;
+//     }
 
-    public static void SetAllEngines(Engine[] engines)
-    {
-        AllEngines = engines;
-    }
+//     public static void SetAllEngines(Engine[] engines)
+//     {
+//         AllEngines = engines;
+//     }
 
-    public static Engine[] GetUnlockedEngines()
-    {
-        return UnlockedEngines;
-    }
+//     public static Engine[] GetUnlockedEngines()
+//     {
+//         return UnlockedEngines;
+//     }
 
-    public static void SetUnlockedEngines(Engine[] engines)
-    {
-        UnlockedEngines = engines;
-    }
+//     public static void SetUnlockedEngines(Engine[] engines)
+//     {
+//         UnlockedEngines = engines;
+//     }
 
-    public static FuelTank[] GetAllFuelTanks()
-    {
-        return AllFuelTanks;
-    }
+//     public static FuelTank[] GetAllFuelTanks()
+//     {
+//         return AllFuelTanks;
+//     }
 
-    public static void SetAllFuelTanks(FuelTank[] tanks)
-    {
-        AllFuelTanks = tanks;
-    }
+//     public static void SetAllFuelTanks(FuelTank[] tanks)
+//     {
+//         AllFuelTanks = tanks;
+//     }
 
-    public static FuelTank[] GetUnlockedFuelTanks()
-    {
-        return UnlockedFuelTanks;
-    }
+//     public static FuelTank[] GetUnlockedFuelTanks()
+//     {
+//         return UnlockedFuelTanks;
+//     }
 
-    public static void SetUnlockedFuelTanks(FuelTank[] tanks)
-    {
-        UnlockedFuelTanks = tanks;
-    }
-}
+//     public static void SetUnlockedFuelTanks(FuelTank[] tanks)
+//     {
+//         UnlockedFuelTanks = tanks;
+//     }
+// }
