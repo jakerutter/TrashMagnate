49dfaac8d9963c4883750eb0789b330989010036
Jake Rutter <rutter.jacob@gmail.com>
2021-06-07 04:53:57 -0700

    making progress on calendar system

---
+++ Assets/Scripts/CalendarMailUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CalendarMailUI : MonoBehaviour
+{
+    
+    public GameObject CalendarIcon;
+    public GameObject MailIcon;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
