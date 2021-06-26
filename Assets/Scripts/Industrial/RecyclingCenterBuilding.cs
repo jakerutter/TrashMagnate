42c1a732a69a94c310ab12ddce7a45032ca96ccf
Jake Rutter <rutter.jacob@gmail.com>
2021-06-26 13:23:49 -0700

    added collider with trigger for store

---
+++ Assets/Scripts/Industrial/RecyclingCenterBuilding.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecyclingCenterBuilding : MonoBehaviour
+{
+    [SerializeField] private GameObject SellUI;
+
+   void OnTriggerEnter(Collider other)
+   {
+       Debug.Log("collided with " + other.name);
+   }
+}
