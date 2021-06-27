3473646f2768e320f4590ae7c7b6282309aaee34
Jake Rutter <rutter.jacob@gmail.com>
2021-06-27 14:09:20 -0700

    added character selection scene

---
+++ Assets/Scripts/CharacterSelection/CharacterSelection.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterSelection : MonoBehaviour
+{
+    public Button previous;
+    public Button next;
+    public Button select;
+
+    public void SelectCharacter()
+    {
+        Debug.Log("Selected character");
+    }
+
+    public void PreviousCharacter()
+    {
+        Debug.Log("Show previous character");
+    }
+
+    public void NextCharacter()
+    {
+        Debug.Log("Show next character");
+    }
+}
