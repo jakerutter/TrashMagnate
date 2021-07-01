679f1563585f56479b1cfcf2a026101325164a07
Jake Rutter <rutter.jacob@gmail.com>
2021-06-30 23:00:44 -0500

    created setPath function for save

--- Assets/Scripts/SaveSystem/SaveSystem.cs
+++ Assets/Scripts/SaveSystem/SaveSystem.cs
@@ -4,9 +4,14 @@ using System.Runtime.Serialization.Formatters.Binary;
 
 public static class SaveSystem
 {
+    public static string specificPath;
+
     public static void SaveGame(DataManager manager)
     {
         BinaryFormatter formatter = new BinaryFormatter();
+        
+        //use specificPath in path to get the save file created by the player
+        //string path = Application.persistentDataPath + specificPath;
         string path = Application.persistentDataPath + "/saveGame.jtr";
 
         FileStream stream = new FileStream(path, FileMode.Create);
@@ -35,5 +40,11 @@ public static class SaveSystem
           Debug.LogError("Save file not found in " + path);
           return null;
         }
+    }
+
+    public static void SetSpecificPath(string path)
+    {
+        //this function is called when a player creates a new save
+        specificPath = path;
     }
 }
